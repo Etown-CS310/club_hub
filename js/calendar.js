8c5159849d9555a82e911f04329e3a5f36ebf4c3
MSmit99 <144396430+MSmit99@users.noreply.github.com>
2024-09-25 11:24:52 -0400

    clean up project / file structure

--- js/calendar.js
+++ js/calendar.js
@@ -5,3 +5,4 @@ document.addEventListener('DOMContentLoaded', function() {
     })
     calendar.render()
   })
+  
--- js/global.js
+++ js/global.js
@@ -1,16 +1,16 @@
-/**
-   * Initialize global components through loadCompenent function
+  /*
+   * Initialize global components through loadComponent function
    */
 async function initGlobal() {
     try {
-        await loadComponent('components/navbar.html', 'navbar');
-        await loadComponent('components/footer.html', 'footer');
+        await loadComponent('/components/navbar.html', 'navbar');
+        await loadComponent('/components/footer.html', 'footer');
     } catch (error) {
         console.error('Error loading components:', error);
     }
 }
 
-/**
+  /*
    * Takes url path and element id to load component
    * @para {string, string} path and element id to load component
    */
